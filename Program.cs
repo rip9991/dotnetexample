462f0838acfa30d0140489f220a9aba3fba5dbfc
rip9991 <rip9991@yahoo.com>
2020-01-20 17:31:25 +0200

    added dotnetexample project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
